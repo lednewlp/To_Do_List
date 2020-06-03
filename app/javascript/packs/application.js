dc2b19993e8de55ce985f849c1a6f05436c27899
Wendel Lopes dos Santos Filho <wendel@pop-os.localdomain>
2020-06-03 16:49:52 -0300

    Cross

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("pooper")
+require("global.js")
 
 $(document).ready(function() {
 
@@ -21,6 +22,8 @@ $(document).ready(function() {
 
   });
 });
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
